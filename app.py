7763d4c8c27880458f640123af7cade058ef0310
ogre2007@github.com <r3le@ya.ru>
2021-10-25 16:50:14 +0300

    divided upload and analyze, added redis integration

--- app.py
+++ app.py
@@ -3,35 +3,57 @@ from flask import Flask, request, redirect
 from werkzeug.exceptions import BadRequest
 from werkzeug.exceptions import HTTPException
 import os
+import redis
+import base64
 
 app = Flask(__name__)
 
+r = redis.Redis.from_url('redis://localhost:6667', db=3, decode_responses=True)
 
-def analyze_data(data, id):
-	r = ''
-	with open(id, 'wb') as f:
+def save_to_db(h, blob):
+    r.hset(h, 'blob', base64.b64encode(blob))
+    return True
+
+def load_from_db(h):
+    return base64.b64decode(r.hget(h, 'blob'))
+
+
+def analyze(data, sha256=''):
+	ret = ''
+	with open(sha256, 'wb') as f:
 		f.write(data)
 	for module in binwalk.scan(id, signature=True):
-	    r += "%s Results for %s:\n" % (module.name, id)
+	    ret += "%s Results for %s:\n" % (module.name, id)
 	    for result in module.results:
-	        r += "\t0x%.8X    %s\n" % (result.offset, result.description)
-	os.remove(id)
-	return r
+	        ret += "\t0x%.8X    %s\n" % (result.offset, result.description)
+	os.remove(sha256)
+	return ret
 
-@app.route('/analyze', methods=['POST'])
-def upload_and_analyze():
+@app.route('/upload', methods=['POST'])
+def upload():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             return BadRequest('No file part')
         file = request.files['file']
         if file:
-            result = analyze_data(file.read(), request.args['id'])
-
-            return (result, 200)
-
+            save_to_db(request.args.get('id'), file.read())
+            return ('File saved', 200)
     return BadRequest('Error') 
 
+@app.route('/analyze')
+def analyze_route():
+	sha256 = request.args.get('id')
+	try:
+		return (r.hget(sha256, 'results'), 200)
+	except:
+		try:
+			results = analyze_data(load_from_db(sha256), sha256)
+			r.hset(sha256, 'results', results)
+			return (results, 200)
+		except Exception as e:
+			print(f'{sha256} blob not found or {e}')
+			return (f'{sha256} blob not found or {e}', 404)			
 
 if __name__ == '__main__':
     
